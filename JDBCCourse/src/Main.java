3573086f87272238038fc415e19bae93d91c3553
Kush Agarwal <kush.301104@gmail.com>
2025-10-24 08:49:38 +0530

    Using Prepared Statements in JDBC

--- JDBCCourse/src/Main.java
+++ JDBCCourse/src/Main.java
@@ -14,21 +14,29 @@ public class Main {
         String url = "jdbc:postgresql://localhost:5432/demo";
         String username = "postgres";
         String password = "postgres";
-        String query = "select * from students";
+//        String query = "select * from students";
+        String query = "INSERT INTO students VALUES(?, ?, ?)";
 
         try {
             Class.forName("org.postgresql.Driver");
             Connection conn = DriverManager.getConnection(url, username, password);
             System.out.println("Connected to database successfully");
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(query);
-            while (rs.next()) {
-                int id = rs.getInt("id");
-                String name = rs.getString("name");
-                int marks = rs.getInt("marks");
+//            Statement stmt = conn.createStatement();
+//            ResultSet rs = stmt.executeQuery(query);
+//            while (rs.next()) {
+//                int id = rs.getInt("id");
+//                String name = rs.getString("name");
+//                int marks = rs.getInt("marks");
+//
+//                System.out.println(id + " " + name + " " + marks);
+//            }
 
-                System.out.println(id + " " + name + " " + marks);
-            }
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setInt(1, 2);
+            ps.setString(2, "Daniel");
+            ps.setInt(3, 45);
+            int count = ps.executeUpdate();
+            System.out.println(count + " rows updated successfully");
         } catch (ClassNotFoundException | SQLException e) {
             throw new RuntimeException(e);
         }
