9bb744c7972db521945c162590339f2270856178
Kush Agarwal <kush.301104@gmail.com>
2025-10-25 18:55:26 +0530

    Added sample methods

--- Spring1/src/main/java/org/example/Laptop.java
+++ Spring1/src/main/java/org/example/Laptop.java
@@ -4,4 +4,8 @@ public class Laptop {
     public Laptop() {
         System.out.println("Laptop constructor called");
     }
+
+    public void compile() {
+        System.out.println("Compiling code...");
+    }
 }
