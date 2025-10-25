dd17a98d0c97b8f9db34c9da19a98f2b1ce98ed4
Kush Agarwal <kush.301104@gmail.com>
2025-10-25 18:55:49 +0530

    Adding reference for reference injection

--- Spring1/src/main/java/org/example/Alien.java
+++ Spring1/src/main/java/org/example/Alien.java
@@ -1,7 +1,8 @@
 package org.example;
 
 public class Alien {
-    int age;
+    private int age;
+    private Laptop laptop;
 
     public Alien() {
         System.out.println("Alien constructor called");
@@ -9,9 +10,22 @@ public class Alien {
 
     public void code() {
         System.out.println("Coding...");
+        laptop.compile();
     }
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Laptop getLaptop() {
+        return laptop;
+    }
+
+    public void setLaptop(Laptop laptop) {
+        this.laptop = laptop;
     }
 }
