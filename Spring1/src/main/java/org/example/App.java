af24226885236f7d9788284f4d95d00a19d740f2
Kush Agarwal <kush.301104@gmail.com>
2025-10-25 18:03:34 +0530

    Created a ApplicationContext using XML

--- Spring1/src/main/java/org/example/App.java
+++ Spring1/src/main/java/org/example/App.java
@@ -1,11 +1,20 @@
 package org.example;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
  */
 public class App {
     public static void main( String[] args ) {
+
+        // Creating a context from XML based configurations.
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
         System.out.println( "Hello World!" );
+        Alien alien = (Alien)context.getBean("alien");
+        alien.code();
     }
 }
