84fdf696434dfe5d2d1d29e5207e034c8b38dc29
Kush Agarwal <kush.301104@gmail.com>
2025-10-25 18:48:54 +0530

    Learnt different bean scopes

--- Spring1/src/main/java/org/example/Alien.java
+++ Spring1/src/main/java/org/example/Alien.java
@@ -1,11 +1,17 @@
 package org.example;
 
 public class Alien {
+    int age;
+
     public Alien() {
         System.out.println("Alien constructor called");
     }
 
     public void code() {
         System.out.println("Coding...");
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
--- Spring1/src/main/java/org/example/App.java
+++ Spring1/src/main/java/org/example/App.java
@@ -12,12 +12,23 @@ public class App {
 
         // Creating a context from XML based configurations.
         // All the beans that are mentioned in the spring.xml file, will be created
-        // while we fetch the context itself.
+        // while we fetch the context itself. (This is true only for the beans with singleton scope)
         // getBean() will just get the reference of that object and return it...
+        // All the beans that are defined with prototype scope are created when the getBean() is called...
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
         System.out.println( "Hello World!" );
 
         Alien alien = (Alien)context.getBean("alien");
-        alien.code();
+        // The setAge will change the age for both the alien and alien1 references, if the
+        // bean scope is set to singleton. As the internal object remains the same.
+        // But as soon as the scope is changed to prototype, the alien and alien1 references
+        // point to 2 different objects..!!
+        alien.setAge(30);
+        System.out.println(alien.age);
+//        alien.code();
+
+        Alien alien1 = (Alien)context.getBean("alien");
+        System.out.println(alien1.age);
+//        alien1.code();
     }
 }
