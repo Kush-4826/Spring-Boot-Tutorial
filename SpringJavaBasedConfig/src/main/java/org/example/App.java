9cf49bdaff25af74d4392dee37693386ad09cbb6
Kush Agarwal <kush.301104@gmail.com>
2025-10-25 19:50:24 +0530

    Creating Context using Java Based Config

--- SpringJavaBasedConfig/src/main/java/org/example/App.java
+++ SpringJavaBasedConfig/src/main/java/org/example/App.java
@@ -1,13 +1,20 @@
 package org.example;
 
+import org.example.config.AppConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         System.out.println( "Hello World!" );
+
+//        Accessing the bean based on the type of the bean
+        Desktop desktop = context.getBean(Desktop.class);
+        desktop.compile();
     }
 }
