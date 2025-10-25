aa7d044890f7bb745de8323121c2cec46c2e01d2
Kush Agarwal <kush.301104@gmail.com>
2025-10-25 19:09:47 +0530

    Created a Parent Interface for Laptop

--- Spring1/src/main/java/org/example/Laptop.java
+++ Spring1/src/main/java/org/example/Laptop.java
@@ -1,11 +1,12 @@
 package org.example;
 
-public class Laptop {
+public class Laptop implements ComputerSystem {
     public Laptop() {
         System.out.println("Laptop constructor called");
     }
 
+    @Override
     public void compile() {
-        System.out.println("Compiling code...");
+        System.out.println("Compiling code using Laptop...");
     }
 }
