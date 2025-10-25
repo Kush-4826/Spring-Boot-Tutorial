3ae63b1e3f3b4c13e5e78c39e0146ff654907b55
Kush Agarwal <kush.301104@gmail.com>
2025-10-25 19:23:32 +0530

    Autowiring Dependencies

--- Spring1/src/main/java/org/example/App.java
+++ Spring1/src/main/java/org/example/App.java
@@ -29,13 +29,21 @@ public class App {
         alien.code();
 
         System.out.println("-------------------------------------------------------------");
+        System.out.println("(Constructor Injection Example)");
         Alien alien1 = (Alien)context.getBean("alien1");
         System.out.println(alien1.getAge());
         alien1.code();
 
         System.out.println("-------------------------------------------------------------");
+        System.out.println("(Interface Example)");
         Alien alien2 = (Alien)context.getBean("alien2");
         System.out.println(alien2.getAge());
         alien2.code();
+
+        System.out.println("-------------------------------------------------------------");
+        System.out.println("(Autowiring Example)");
+        Alien alien3 = (Alien)context.getBean("alien3");
+        System.out.println(alien3.getAge());
+        alien3.code();
     }
 }
