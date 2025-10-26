cff465a9af96ea1c463b064b7044950e50e26926
Kush Agarwal <kush.301104@gmail.com>
2025-10-26 22:05:53 +0530

    New Route for adding and displaying result

--- SpringBootWeb1/src/main/java/org/example/springbootweb1/HomeController.java
+++ SpringBootWeb1/src/main/java/org/example/springbootweb1/HomeController.java
@@ -1,5 +1,7 @@
 package org.example.springbootweb1;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,5 +11,15 @@ public class HomeController {
     @RequestMapping("/")
     public String index(){
         return "index.jsp";
+    }
+
+    @RequestMapping("add")
+    public String add(HttpServletRequest request, HttpSession session) {
+
+        int num1 = Integer.parseInt(request.getParameter("num1"));
+        int num2 = Integer.parseInt(request.getParameter("num2"));
+        int res = num1 + num2;
+        session.setAttribute("res", res);
+        return "add.jsp";
     }
 }
---
+++ SpringBootWeb1/src/main/webapp/add.jsp
@@ -0,0 +1,10 @@
+<%@page language="java" %>
+
+<html>
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Result: <%= session.getAttribute("res") %></h1>
+</body>
+</html>
