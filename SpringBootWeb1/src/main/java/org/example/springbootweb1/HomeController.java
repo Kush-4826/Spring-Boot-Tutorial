108e4036e46880375fc52f8ca84ce8f47acc2f6c
Kush Agarwal <kush.301104@gmail.com>
2025-10-27 00:18:36 +0530

    Used ModelAttribute

--- SpringBootWeb1/src/main/java/org/example/springbootweb1/HomeController.java
+++ SpringBootWeb1/src/main/java/org/example/springbootweb1/HomeController.java
@@ -1,6 +1,7 @@
 package org.example.springbootweb1;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -23,6 +24,26 @@ public class HomeController {
 //        return "add";
 
         modelAndView.addObject("res", res);
+        modelAndView.setViewName("add");
+        return modelAndView;
+    }
+
+//    @RequestMapping("addAlien")
+//    public ModelAndView addAlien(@RequestParam("id") int id, @RequestParam("name") String name, ModelAndView modelAndView) {
+//
+//        Alien alien = new Alien(id, name);
+//
+//        modelAndView.addObject("alien", alien);
+//        modelAndView.setViewName("add");
+//        return modelAndView;
+//    }
+
+    @RequestMapping("addAlien")
+    public ModelAndView addAlien(@ModelAttribute Alien alien, ModelAndView modelAndView) {
+
+//        Alien alien = new Alien(id, name);
+//
+//        modelAndView.addObject("alien", alien);
         modelAndView.setViewName("add");
         return modelAndView;
     }
