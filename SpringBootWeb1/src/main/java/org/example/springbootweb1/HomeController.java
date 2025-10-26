9633546b35994dca5e09eeb1ae064b2b37f0be86
Kush Agarwal <kush.301104@gmail.com>
2025-10-26 22:13:41 +0530

    Using RequestParams

--- SpringBootWeb1/src/main/java/org/example/springbootweb1/HomeController.java
+++ SpringBootWeb1/src/main/java/org/example/springbootweb1/HomeController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -14,11 +15,11 @@ public class HomeController {
     }
 
     @RequestMapping("add")
-    public String add(HttpServletRequest request, HttpSession session) {
+    public String add(@RequestParam("num1") int num, int num2, HttpSession session) {
 
-        int num1 = Integer.parseInt(request.getParameter("num1"));
-        int num2 = Integer.parseInt(request.getParameter("num2"));
-        int res = num1 + num2;
+//        int num1 = Integer.parseInt(request.getParameter("num1"));
+//        int num2 = Integer.parseInt(request.getParameter("num2"));
+        int res = num + num2;
         session.setAttribute("res", res);
         return "add.jsp";
     }
