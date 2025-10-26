22ec9a39cef618267555ce639d3913cf6a65d904
Kush Agarwal <kush.301104@gmail.com>
2025-10-26 12:05:43 +0530

    Used JDBCTemplate to insert the data into H2 Database

--- SpringJDBC/src/main/java/org/example/springjdbc/repo/StudentRepository.java
+++ SpringJDBC/src/main/java/org/example/springjdbc/repo/StudentRepository.java
@@ -1,6 +1,8 @@
 package org.example.springjdbc.repo;
 
 import org.example.springjdbc.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -9,8 +11,21 @@ import java.util.List;
 @Repository
 public class StudentRepository {
 
+    private JdbcTemplate jdbcTemplate;
+
+    public JdbcTemplate getJdbcTemplate() {
+        return jdbcTemplate;
+    }
+
+    @Autowired
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     public void save(Student student) {
-        System.out.println(student + " added");
+        String sql = "INSERT INTO students(roll_no, name, marks) VALUES(?, ?, ?)";
+        int rows = jdbcTemplate.update(sql, student.getRollNo(), student.getName(), student.getMarks());
+        System.out.println(rows + " rows inserted");
     }
 
     public List<Student> findAll() {
