a64f16787d8d766c66a9f64a6a3f9c509851fbf0
Kush Agarwal <kush.301104@gmail.com>
2025-10-26 11:48:48 +0530

    Using the Student Model Bean

--- SpringJDBC/src/main/java/org/example/springjdbc/SpringJdbcApplication.java
+++ SpringJDBC/src/main/java/org/example/springjdbc/SpringJdbcApplication.java
@@ -1,13 +1,19 @@
 package org.example.springjdbc;
 
+import org.example.springjdbc.model.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringJdbcApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringJdbcApplication.class, args);
+        ApplicationContext context = SpringApplication.run(SpringJdbcApplication.class, args);
+        Student s = context.getBean(Student.class);
+        s.setRollNo(1);
+        s.setName("Kush");
+        s.setMarks(100);
     }
 
 }
