a2a1f5584bb00573e13ee18ad1c76c3b68e707b3
Kush Agarwal <kush.301104@gmail.com>
2025-10-27 00:48:26 +0530

    New Project - Job App

---
+++ JobAppProject/src/main/java/org/example/jobapp/JobAppProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.jobapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobAppProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobAppProjectApplication.class, args);
+    }
+
+}
---
+++ JobAppProject/src/test/java/org/example/jobapp/JobAppProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jobapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAppProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
