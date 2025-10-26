9c6295219dbd841724657f9812fe46c413395829
Kush Agarwal <kush.301104@gmail.com>
2025-10-26 11:29:24 +0530

    Using the laptopRepository

--- SpringBootDemo2/src/main/java/com/example/app/service/LaptopService.java
+++ SpringBootDemo2/src/main/java/com/example/app/service/LaptopService.java
@@ -1,11 +1,20 @@
 package com.example.app.service;
 
 import com.example.app.model.Laptop;
+import com.example.app.repo.LaptopRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LaptopService {
+
+    private final com.example.app.repo.LaptopRepository laptopRepository;
+
+    public LaptopService(LaptopRepository laptopRepository) {
+        this.laptopRepository = laptopRepository;
+    }
+
     public void addLaptop(Laptop laptop) {
         System.out.println("addLaptop called");
+        laptopRepository.save(laptop);
     }
 }
