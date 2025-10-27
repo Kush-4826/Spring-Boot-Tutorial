b86ed9d93d86c38b5d40f3224c096bbe170491e5
Kush Agarwal <kush.301104@gmail.com>
2025-10-27 11:12:51 +0530

    Using Repo Layer to access the data

--- JobAppProject/src/main/java/org/example/jobapp/service/JobService.java
+++ JobAppProject/src/main/java/org/example/jobapp/service/JobService.java
@@ -1,17 +1,26 @@
 package org.example.jobapp.service;
 
 import org.example.jobapp.model.JobPost;
+import org.example.jobapp.repo.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class JobService {
-    public void addJob(JobPost jobPost) {
+    private final JobRepository jobRepository;
 
+    @Autowired
+    public JobService(JobRepository jobRepository) {
+        this.jobRepository = jobRepository;
+    }
+
+    public void addJob(JobPost jobPost) {
+        this.jobRepository.addJob(jobPost);
     }
 
     public List<JobPost> getJobs() {
-        return null;
+        return this.jobRepository.getJobs();
     }
 }
