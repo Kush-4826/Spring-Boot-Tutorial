e7712a6d325496774b5143b53aedbf3fc96ad24b
Kush Agarwal <kush.301104@gmail.com>
2025-10-27 11:12:34 +0530

    Using Service Layer to perform processing

--- JobAppProject/src/main/java/org/example/jobapp/controller/JobController.java
+++ JobAppProject/src/main/java/org/example/jobapp/controller/JobController.java
@@ -1,13 +1,25 @@
 package org.example.jobapp.controller;
 
 import org.example.jobapp.model.JobPost;
+import org.example.jobapp.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class JobController {
+
+    private final JobService jobService;
+
+    @Autowired
+    public JobController(JobService jobService) {
+        this.jobService = jobService;
+    }
 
     @RequestMapping({"/", "home"})
     public String index(){
@@ -20,12 +32,15 @@ public class JobController {
     }
 
     @GetMapping("viewalljobs")
-    public String viewAllJobs(){
+    public String viewAllJobs(Model model){
+        List<JobPost> jobPosts = this.jobService.getJobs();
+        model.addAttribute("jobPosts", jobPosts);
         return "viewalljobs";
     }
 
     @PostMapping("handleForm")
     public String handleForm(JobPost jobPost) {
+        this.jobService.addJob(jobPost);
         return "success";
     }
 }
