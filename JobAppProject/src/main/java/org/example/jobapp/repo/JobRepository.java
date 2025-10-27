5d98fbb26fe33bc58bcab5ba2f5fae6f508c1407
Kush Agarwal <kush.301104@gmail.com>
2025-10-27 11:00:17 +0530

    Created Service and Repo Layers

---
+++ JobAppProject/src/main/java/org/example/jobapp/repo/JobRepository.java
@@ -0,0 +1,8 @@
+package org.example.jobapp.repo;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class JobRepository {
+
+}
---
+++ JobAppProject/src/main/java/org/example/jobapp/service/JobService.java
@@ -0,0 +1,17 @@
+package org.example.jobapp.service;
+
+import org.example.jobapp.model.JobPost;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class JobService {
+    public void addJob(JobPost jobPost) {
+
+    }
+
+    public List<JobPost> getJobs() {
+        return null;
+    }
+}
