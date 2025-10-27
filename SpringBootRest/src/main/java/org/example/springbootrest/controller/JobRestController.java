ec5f43ffa1b43f29d12308c49602ce3dd7401dcc
Kush Agarwal <kush.301104@gmail.com>
2025-10-27 11:58:30 +0530

    Added Frontend as an Origin

--- SpringBootRest/src/main/java/org/example/springbootrest/controller/JobRestController.java
+++ SpringBootRest/src/main/java/org/example/springbootrest/controller/JobRestController.java
@@ -3,12 +3,14 @@ package org.example.springbootrest.controller;
 import org.example.springbootrest.model.JobPost;
 import org.example.springbootrest.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class JobRestController {
 
     private final JobService jobService;
@@ -18,7 +20,7 @@ public class JobRestController {
         this.jobService = jobService;
     }
 
-    @GetMapping("jobposts")
+    @GetMapping("jobPosts")
     public List<JobPost> getAllJobs() {
         return this.jobService.getJobs();
     }
