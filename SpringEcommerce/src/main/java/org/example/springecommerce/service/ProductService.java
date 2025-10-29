16ffefb5c4141edb9a3d704fcc0f6103c0ad4ba5
Kush Agarwal <kush.301104@gmail.com>
2025-10-29 15:41:59 +0530

    Created Product Service

---
+++ SpringEcommerce/src/main/java/org/example/springecommerce/service/ProductService.java
@@ -0,0 +1,20 @@
+package org.example.springecommerce.service;
+
+import org.example.springecommerce.model.Product;
+import org.example.springecommerce.repo.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProducts() {
+        return this.productRepository.findAll();
+    }
+}
