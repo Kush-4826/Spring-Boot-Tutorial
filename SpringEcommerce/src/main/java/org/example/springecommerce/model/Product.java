9de5ae63e7108e603f025a23786feb49f97ad3a0
Kush Agarwal <kush.301104@gmail.com>
2025-10-29 15:20:53 +0530

    Created Product Model

---
+++ SpringEcommerce/src/main/java/org/example/springecommerce/model/Product.java
@@ -0,0 +1,32 @@
+package org.example.springecommerce.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity(name = "products")
+@Component
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String  id;
+    private String name;
+    private String description;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+    private Date releaseDate;
+    private boolean productAvailable;
+    private int stockQuantity;
+}
