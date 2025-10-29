46f391541716fbd9a69156f460eb438ee226cc1e
Kush Agarwal <kush.301104@gmail.com>
2025-10-29 15:42:56 +0530

    Created index method to get all products

--- SpringEcommerce/src/main/java/org/example/springecommerce/controller/ProductController.java
+++ SpringEcommerce/src/main/java/org/example/springecommerce/controller/ProductController.java
@@ -1,12 +1,23 @@
 package org.example.springecommerce.controller;
 
+import org.example.springecommerce.model.Product;
+import org.example.springecommerce.service.ProductService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class ProductController extends ApiController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        super();
+        this.productService = productService;
+    }
+
     @GetMapping("products")
-    public String index() {
-        return "Hello World";
+    public List<Product> index() {
+        return this.productService.getAllProducts();
     }
 }
