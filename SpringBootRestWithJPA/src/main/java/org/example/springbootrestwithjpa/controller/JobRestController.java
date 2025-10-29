a221324338fb5f0b7a22e47c4d16c1e1fcac03fb
Kush Agarwal <kush.301104@gmail.com>
2025-10-29 13:36:30 +0530

    Using application.properties to manage CORS URL

--- SpringBootRestWithJPA/src/main/java/org/example/springbootrestwithjpa/controller/JobRestController.java
+++ SpringBootRestWithJPA/src/main/java/org/example/springbootrestwithjpa/controller/JobRestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "${client.url}")
 public class JobRestController {
 
     private final JobService jobService;
