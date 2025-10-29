a395cca2774565c644ab05cf7a6b108ae6064fa6
Kush Agarwal <kush.301104@gmail.com>
2025-10-29 15:42:28 +0530

    Added CORS

--- SpringEcommerce/src/main/java/org/example/springecommerce/controller/ApiController.java
+++ SpringEcommerce/src/main/java/org/example/springecommerce/controller/ApiController.java
@@ -1,9 +1,11 @@
 package org.example.springecommerce.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "${client.url}")
 public class ApiController {
 }
