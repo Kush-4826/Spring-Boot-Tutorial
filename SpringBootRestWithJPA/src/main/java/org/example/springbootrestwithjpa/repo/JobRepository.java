f6127a54a47335480ba83de3240de35cd42eadce
Kush Agarwal <kush.301104@gmail.com>
2025-10-29 13:34:19 +0530

    Added Method to search

--- SpringBootRestWithJPA/src/main/java/org/example/springbootrestwithjpa/repo/JobRepository.java
+++ SpringBootRestWithJPA/src/main/java/org/example/springbootrestwithjpa/repo/JobRepository.java
@@ -4,7 +4,10 @@ import org.example.springbootrestwithjpa.model.JobPost;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface JobRepository  extends JpaRepository<JobPost, Integer> {
 
+    List<JobPost> findJobPostsByPostProfileOrPostDescContains(String postProfile, String postDesc);
 }
