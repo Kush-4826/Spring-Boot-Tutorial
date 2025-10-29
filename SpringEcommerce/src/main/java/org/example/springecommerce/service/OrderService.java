9273149a3ebb8202ca82ff16f37a77fcc01d3cb5
Kush Agarwal <kush.301104@gmail.com>
2025-10-29 19:10:01 +0530

    Created Orders Controller

---
+++ SpringEcommerce/src/main/java/org/example/springecommerce/service/OrderService.java
@@ -0,0 +1,19 @@
+package org.example.springecommerce.service;
+
+import org.example.springecommerce.model.dto.OrderRequest;
+import org.example.springecommerce.model.dto.OrderResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderService {
+    public OrderResponse placeOrder(OrderRequest orderRequest) {
+
+        return null;
+    }
+
+    public List<OrderResponse> getAllOrderResponses() {
+        return null;
+    }
+}
