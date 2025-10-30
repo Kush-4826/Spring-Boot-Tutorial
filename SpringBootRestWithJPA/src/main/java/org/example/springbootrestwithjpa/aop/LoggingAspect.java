1a67e7137eb5d53c884615e10946b6f380dcf1e9
Kush Agarwal <kush.301104@gmail.com>
2025-10-30 17:26:46 +0530

    Created an Aspect to log before every service method call

---
+++ SpringBootRestWithJPA/src/main/java/org/example/springbootrestwithjpa/aop/LoggingAspect.java
@@ -0,0 +1,18 @@
+package org.example.springbootrestwithjpa.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LoggingAspect {
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
+
+    @Before(value = "execution(* org.example.springbootrestwithjpa.service.JobService.*(..))")
+    public void call() {
+        LOGGER.info("Call AOP ...");
+    }
+}
