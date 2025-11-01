88a5f66b2a0bf65c8ad0478e36026e358408210f
Kush Agarwal <kush.301104@gmail.com>
2025-11-01 20:45:52 +0530

    Created a Login Route

--- SpringSecurity/src/main/java/org/example/springsecurity/config/SecurityConfig.java
+++ SpringSecurity/src/main/java/org/example/springsecurity/config/SecurityConfig.java
@@ -3,9 +3,12 @@ package org.example.springsecurity.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -35,12 +38,17 @@ public class SecurityConfig {
 
         http.csrf(customizer -> customizer.disable());
         http.authorizeHttpRequests(requests -> requests
-                .requestMatchers("/register").permitAll()
+                .requestMatchers("/register", "login").permitAll()
                 .anyRequest()
                 .authenticated());
         http.httpBasic(Customizer.withDefaults());
         http.logout(Customizer.withDefaults());
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
--- SpringSecurity/src/main/java/org/example/springsecurity/controller/UserController.java
+++ SpringSecurity/src/main/java/org/example/springsecurity/controller/UserController.java
@@ -3,6 +3,10 @@ package org.example.springsecurity.controller;
 import org.example.springsecurity.model.User;
 import org.example.springsecurity.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +16,21 @@ public class UserController {
     @Autowired
     public UserService userService;
 
+    @Autowired
+    AuthenticationManager authenticationManager;
+
     @PostMapping("register")
     public User register(@RequestBody User user) {
         return this.userService.saveUser(user);
     }
 
+    @PostMapping("login")
+    public String login(@RequestBody User user) {
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+        if(authentication.isAuthenticated()) {
+            return "success";
+        }
+        return "fail";
+    }
 }
