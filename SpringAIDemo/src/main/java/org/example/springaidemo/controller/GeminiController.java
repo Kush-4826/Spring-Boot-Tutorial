949087ac1e8b72a78df8448187db03eb9ba389ad
Kush Agarwal <kush.301104@gmail.com>
2025-11-06 00:41:27 +0530

    Adding Memory to the ChatClient

--- SpringAIDemo/src/main/java/org/example/springaidemo/controller/GeminiController.java
+++ SpringAIDemo/src/main/java/org/example/springaidemo/controller/GeminiController.java
@@ -1,17 +1,19 @@
 package org.example.springaidemo.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.google.genai.GoogleGenAiChatModel;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public class GeminiController {
     private ChatClient chatClient;
+    ChatMemory chatMemory = MessageWindowChatMemory.builder().build();
 
 //    public GeminiController(GoogleGenAiChatModel chatModel) {
 //        this.chatClient = ChatClient.create(chatModel);
@@ -19,13 +21,25 @@ public class GeminiController {
 
     // Automatically builds the Chat client based on the api key you set in application.properties
     public GeminiController(ChatClient.Builder builder) {
-        this.chatClient = builder.build();
+        this.chatClient = builder
+                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
+                .build();
     }
 
     @GetMapping("/api/gemini/ask")
     public ResponseEntity<String> greet(@RequestParam String q) {
         ChatResponse chatResponse = chatClient
                 .prompt("Answer the following question in one precise line.. Do not add anything else in the response. " + q)
+                .call()
+                .chatResponse();
+        String response = chatResponse.getResult().getOutput().getText();
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/api/gemini")
+    public ResponseEntity<String> ask(@RequestParam String q) {
+        ChatResponse chatResponse = chatClient
+                .prompt(q)
                 .call()
                 .chatResponse();
         String response = chatResponse.getResult().getOutput().getText();
